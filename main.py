e54c46fa82429b7fc288c9a69d1d6fcf4bc1d7bd
Archie Espelimbergo Cortez <archie.cortez@outlook.com>
2025-07-24 10:33:03 +0800

    updated

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-from dahua_ipc import *
 from dotenv import load_dotenv
 import os
 import argparse
 
+from dahua_ipc.dahua import DahuaCameraAPI
+
 load_dotenv()
 
 if __name__ == "__main__":
-    dahua = DahuaIPC(
+    dahua = DahuaCameraAPI(
         os.environ.get("CAM_IP"),
         os.environ.get("CAM_USER"),
         os.environ.get("CAM_PASS"),
     )
 
-    # response = dahua.GetVideoInOptionsConfig()
-    # print("GetVideoInOptionsConfig() response:")
-    # print(response)
     
-    
-    response = dahua.GetVideoColorConfig()
-    print("GetVideoColorConfig() response:")
-    print(response)
     
     
