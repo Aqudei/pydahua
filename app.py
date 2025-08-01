3911805abcfe525eba23c0592c32edb308153b5a
aqudei <fdeu.aalmine@gmail.com>
2025-08-02 07:39:56 +0800

    upda

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()  # Optional; explicit loading if running via `python app.py`
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_cors import cross_origin
 from .dahua_ipc.dahua import DahuaCameraAPI
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Optional; explicit loading if running via `python app.py`
-
 
 CAMERA_IP = os.getenv("CAMERA_IP")
 CAMERA_USER = os.getenv("CAMERA_USER")
 CAMERA_PASS = os.getenv("CAMERA_PASS")
+
+print(CAMERA_IP, CAMERA_USER, CAMERA_PASS)
 
 cam = DahuaCameraAPI(CAMERA_IP, CAMERA_USER, CAMERA_PASS)
 
